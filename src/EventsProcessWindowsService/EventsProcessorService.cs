2af6ac1f0f27d6f34dfa27653141aa5f97c4f169
langelov <lyubomir.angelov@telerik.com>
2024-03-27 19:35:39 +0200

    Add project for the windows service

---
+++ src/EventsProcessWindowsService/EventsProcessorService.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace EventsProcessWindowsService
+{
+    public partial class EventsProcessorService : ServiceBase
+    {
+        private IConnection connection;
+        private IModel channel;
+
+        public EventsProcessorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                Port = 5672,
+                RequestedHeartbeat = TimeSpan.FromSeconds(30)
+            };
+
+            this.connection = factory.CreateConnection();
+            this.channel = this.connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                string messageBody = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // Dowork
+                Thread.Sleep(1000);
+            };
+
+            channel.BasicConsume(queue: "eventsQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+
+        protected override void OnStop()
+        {
+            this.channel.Dispose();
+            this.connection.Dispose();
+        }
+    }
+}
