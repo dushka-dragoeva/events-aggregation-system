3788b64c8fe7d8cebd23e10af5aae7c2f71d2348
langelov <lyubomir.angelov@telerik.com>
2024-03-28 15:30:41 +0200

    Add script to import rabbit mq definitions.

---
+++ ConfigureRabbitMQ.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\RabbitMQ Server\rabbitmq_server-3.12.6\sbin\"rabbitmqctl stop_app
+"C:\Program Files\RabbitMQ Server\rabbitmq_server-3.12.6\sbin\"rabbitmq-plugins enable rabbitmq_management
+"C:\Program Files\RabbitMQ Server\rabbitmq_server-3.12.6\sbin\"rabbitmqctl start_app
+"C:\Program Files\RabbitMQ Server\rabbitmq_server-3.12.6\sbin\"rabbitmqctl import_definitions rabbit_definitions.json
